8d55ee17eaa45e83f6af1a8c637d46cdc04bbb64
juanyachino <juanyachino@gmail.com>
2020-04-02 12:45:20 -0300

    updated app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,28 @@
 class App < Sinatra::Base
   get "/" do
-    "hello cruel world!!!"
+    erb :index
   end
+  get "/users" do
+  	logger.info '/users'
+  	logger.info params
+  	logger.info '----'
+  end
+  get "/hello/:name" do
+  	"Hi #{params['name']}"
+
+  end
+  post "/users/add" do
+  	logger.info "----"
+  	logger.info params
+  	logger.info JSON.parse(request.body.read)
+  	logger.info "------"
+  	
+  end
+  get "/posts" do
+  	# matches "GET /posts?title=foo&author=bar"
+  	title = params["title"]
+  	author = params["author"]
+  	# uses title and author variables:query is optional to the /posts route
+  end
+ 
 end
