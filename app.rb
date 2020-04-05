ff43d4209b2741e415a7bc03381ab7b0df107364
Franco Brusatti <fbrusatti@gmail.com>
2020-04-05 13:55:02 -0300

    Allow configure system using config.yml file

--- app.rb
+++ app.rb
@@ -1,17 +1,51 @@
 require 'sinatra/base'
+require "sinatra/config_file"
 
 class App < Sinatra::Base
+  register Sinatra::ConfigFile
+
+  config_file 'config/config.yml'
+
   configure :development, :production do
     enable :logging
   end
 
+  # Shows how to access to settings configurations
   get "/" do
-    "hello cruel world!!!"
+    logger.info "params"
+    logger.info params
+    logger.info "--------------"
+
+    logger.info "Configurations"
+    logger.info settings.db_adapter
+    logger.info "--------------"
+
+    @prueba = "CHAU"
+
+    erb :index
   end
 
-  post "/users" do
-    # Crear el usuario en DB posiblemente
+  # Shows how to grab a path params
+  get "/hello/:name" do
+    "Hello #{params['name']}"
+  end
 
+  # Returns information to user :id
+  get "/users/:id" do
+    logger.info "/users/:id"
+    logger.info params
+    logger.info "----"
+  end
+
+  # Lists all users (usually called user index)
+  get "/users" do
+    logger.info "/users"
+    logger.info params
+    logger.info "----"
+  end
+
+  # Create an user
+  post "/users" do
     logger.info "--------"
     logger.info params
     logger.info JSON.parse(request.body.read)
