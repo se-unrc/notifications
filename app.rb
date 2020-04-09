eccdf647a1fcae88f70c66c508c898a806703114
Juan <juanito_alanis@hotmail.com>
2020-04-09 18:45:17 -0300

    update

--- app.rb
+++ app.rb
@@ -5,15 +5,35 @@ class App < Sinatra::Base
   end
 
   get "/aboutus" do
-	erb :aboutus
+	 erb :aboutus
   end
 
   get "/login" do
-	erb :login
+	 erb :login
   end	
 	 
   get "/signup" do
-	erb :signup
+	 erb :signup
   end	
+
+
+  post '/login' do
+    if  params[:username] == "juanalanis" #funcion que busque si password corresponde a ese username
+      "My name is #{params[:username]}, and my password is #{params[:password]}"
+    else 
+      erb :login
+    end
+    
+  
+  end
+
+
+  post '/signup' do
+    if params[:username] == "juanalanis" || params[:username] == "jereparla" #funcion que busque si el nombre usuario o email ya fueron usados
+      erb :signup 
+    else
+      "My name is Juan"
+    end
+  end
 
 end 
