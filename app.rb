1909c94e6bb31d0251cd5cbf464786a23d024dcb
Juan <juanito_alanis@hotmail.com>
2020-04-10 19:25:42 -0300

    logical and visual update

--- app.rb
+++ app.rb
@@ -1,6 +1,4 @@
 class App < Sinatra::Base
-
-  redirectedSignup = 0
  
   get "/" do 
   	erb :docs
@@ -23,25 +21,42 @@ class App < Sinatra::Base
     erb :forgotpass
   end
 
+  get "/profile" do
+    erb :profile
+  end 
 
   post '/login' do
     if  params[:username] == "juanalanis" #funcion que busque si password corresponde a ese username
       "Succesful log in" #TODO: direct to docs homepage as registered user
     else 
-        erb :failedLogin
+        @error = 'Username or password was incorrect'
+        erb :login
     end
   end
 
 
   post '/signup' do
     if params[:username] == "juanalanis"  #funcion que busque si el nombre usuario o email ya fueron usados
-      erb :failedSignupUser
+        @error = 'The username is invalid'
+        erb :signup
     elsif params[:email] == "juan@hola.com"
-      erb :failedSignupEmail
+        @error = 'The email is invalid'
+        erb :signup
     elsif params[:password] != params[:confPassword]
-      erb :failedSignupPass
+        @error = 'Passwords do not match'
+        erb :signup
     else
       "Succesful sign up"
+    end
+  end
+
+  post '/profile' do
+    if params[:categorie] == "presupuesto"
+      @error ="You are already suscribed to #{params[:categorie]}"
+      erb :profile
+    else
+      @success ="Now you are suscribed to #{params[:categorie]}!"
+      erb :profile
     end
   end
 
