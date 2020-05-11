5090256f028eb61746aa7ae0cac9298b1be0f7a0
nicoRaftiSoto <nicoraftisoto@gmail.com>
2020-05-10 21:03:49 -0300

    agregado app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
-class App < Sinatra::Base
-  get "/" do
+get "/" do #listo
     erb:Login
   end
-  get "/Admin" do
+  get "/NewUser" do #listo
+    erb:NewUser
+  end
+  get "/User" do #listo
+    erb:User
+  end
+  get "/Admin" do #listo
     erb:Admin
   end
-end
+  get "/NewDocument" do
+    erb:NewDocument
+  end
