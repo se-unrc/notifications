4d06643c3429301cb25b3c919fa6eeccb6b264cf
ebaez <ebaez@it10coop.com.ar>
2020-05-11 12:47:43 -0300

    Add session, clean code

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require "sinatra/config_file"
 require './models/user.rb'
+
 class App < Sinatra::Base
   register Sinatra::ConfigFile
 
@@ -8,87 +9,126 @@ class App < Sinatra::Base
 
   configure :development, :production do
     enable :logging
+    enable :sessions
+    set :session_secret, "So0perSeKr3t!"
+    set :sessions, true
   end
-  
+
+  use Rack::Session::Pool, :expire_after => 2592000
+
   get "/" do
-    erb :index, :layout => :layout
+    logger.info "Session inicilized"
+    logger.info session["session_id"]
+    logger.info session.inspect
+    logger.info session[:user_id]
+    logger.info "--------------"
+    logger.info ""
+  	erb :index, :layout => :layout
   end
+
+  # Add new user
   get "/register" do
-    erb :register
-  end
-  post '/register' do
-   request.body.rewind
-    hash = Rack::Utils.parse_nested_query(request.body.read)
-    params = JSON.parse hash.to_json 
-    user = User.new(name: params["name"], email: params["email"], username: params["username"], password: params["psw"])
-    if user.save
-      redirect "/login"
+    if !session[:user_id]
+      erb :register
     else
-      [500, {}, "Internal Server Error"]
+      redirect "/profile"
     end
   end
-  post '/tag' do
-    erb :tag
+
+  post '/register' do
+   request.body.rewind
+   hash = Rack::Utils.parse_nested_query(request.body.read)
+   params = JSON.parse hash.to_json
+   user = User.new(name: params["name"], email: params["email"], username: params["username"], password: params["psw"])
+   if user.save
+    redirect "/login"
+   else
+    [500, {}, "Internal Server Error"]
+   end
   end
+
+  # Login Endpoints
+  get "/login" do
+    if !session[:user_id]
+      erb :login
+    else
+      redirect "/profile"
+    end
+  end
+
+  post '/login' do
+    users = User.find(username: params[:username])
+    if users && users.password == params[:password]
+      session[:user_id] = users.id
+      redirect "/profile"
+    else
+      erb :login
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    # response.set_cookie("user_id", value: "", expires: Time.now - 100 )
+    redirect '/'
+  end
+
+  #############################
   get '/registersuccess' do
     erb :registerlandingpage
   end
-  post '/loginsuccess' do
-    erb :loginlandingpage, :layout => :layoutlogin
+
+  post '/sign_in' do
+    if User.last.id
+      session[:user_id] = User.last.id
+      [200, {"Content-Type" => "text/plain"}, ["You're logged in"]]
+    else
+      # halt 401, 'go away!'
+      [400, {"Content-Type" => "text/plain"}, ["Unautorized"]]
+    end
   end
-  get "/login" do
-    erb :login
+
+  # Endpoints for handles profile
+  get "/profile" do
+    erb :perfil , :layout => :layoutlogin
   end
-  get '/login' do
-    erb :login
-  end
-  get "/upload" do
-  	erb :upload
-  end
-  get "/aboutus" do
-    erb :aboutus
-  end
-  get "/contactus" do
-    erb :contactus
-  end
-  get "/perfil" do
+
+  post '/profile' do
     erb :perfil, :layout => :layoutlogin
   end
-  post '/perfil' do
-    erb :perfil, :layout => :layoutlogin
+
+  # Endpoints for upload a document
+  get '/documents' do
+    erb :upload
   end
+
+  get '/showdocument' do
+    erb :show_file
+  end
+
   post '/save_documents' do
     @filename = params[:file][:filename]
     file = params[:file][:tempfile]
     File.open("./public/#{@filename}", 'wb') do |f|
-    f.write(file.read)
+      f.write(file.read)
     end
     erb :tag
   end
+
+  ###
   get "/tos" do
   	erb :ToS
   end
-  get "/users" do
-  	logger.info '/users'
-  	logger.info params
-  	logger.info '----'
-  end
-  get "/hello/:name" do
-  	"Hi #{params['name']}"
 
+  get "/aboutus" do
+    erb :aboutus
   end
-  post "/users/add" do
-  	logger.info "----"
-  	logger.info params
-  	logger.info JSON.parse(request.body.read)
-  	logger.info "------"
-  	
+
+  get "/contactus" do
+    erb :contactus
   end
-  get "/posts" do
-  	# matches "GET /posts?title=foo&author=bar"
-  	title = params["title"]
-  	author = params["author"]
-  	# uses title and author variables:query is optional to the /posts route
+
+  post '/tag' do
+      erb :tag
   end
- 
+
 end
--- models/user.rb
+++ models/user.rb
@@ -1,2 +1,8 @@
 class User < Sequel::Model
+	plugin :validation_helpers
+  def validate
+    super
+    validates_presence [:name, :email, :username, :password]
+    validates_unique [:username]
+  end
 end
