ddf3f4dd83c659bb82c4d0d990070a29fd009c39
nicoRaftiSoto <nicoraftisoto@gmail.com>
2020-05-10 21:14:39 -0300

    :arreglado el app.rb

--- app.rb
+++ app.rb
@@ -1,15 +1,16 @@
-get "/" do #listo
-    erb:Login
-  end
-  get "/NewUser" do #listo
-    erb:NewUser
-  end
-  get "/User" do #listo
-    erb:User
-  end
-  get "/Admin" do #listo
-    erb:Admin
-  end
-  get "/NewDocument" do
-    erb:NewDocument
-  end
+class App < Sinatra::Base
+  get "/" do #listo
+      erb:Login
+    end
+    get "/NewUser" do #listo
+      erb:NewUser
+    end
+    get "/User" do #listo
+      erb:User
+    end
+    get "/Admin" do #listo
+      erb:Admin
+    end
+    get "/NewDocument" do
+      erb:NewDocument
+    end
