c756b6905564309096b535a41466d5c5ae8269f4
ebaez <ebaez@it10coop.com.ar>
2020-09-15 01:14:32 -0300

    [finishes #174681979]Agregue tests en test_document

--- test/models/test_document.rb
+++ test/models/test_document.rb
@@ -13,8 +13,25 @@ class DocumentTest < Minitest::Unit::TestCase
 		assert_equal @document.valid?, false
 	end
 
-	def test_create_document
-  		user  = User.new(name: "testUser",email: "test@test.com", username: "test",password: "123456")
-  		assert_equal user.valid?, true
+	def test_create_invalid_document
+  		@document  = Document.new(name:"" , date:""  , uploader:"" , subject:"" )
+  		assert_equal @document.valid?, false
 	end
+
+	def test_create_document
+		@document = Document.new(name:"test.pdf" , date:"14-09-2020"  , uploader:"userTest" , subject:"Coment test" )
+		assert_equal @document.valid?, true
+	end 
+
+	def test_create_invalid_field_document 
+		@document = Document.new(date:"14-09-2020"  , uploader:"userTest" , subject:"Coment test" )
+		assert_equal @document.valid?, false
+	end
+
+	def test_format_file_document
+		@document = Document.new(name:"test.pgn" , date:"14-09-2020"  , uploader:"userTest" , subject:"Coment test" )
+		assert_equal @document.valid?, false
+	end	
+
+		
 end
